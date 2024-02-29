ca20921ec1b40815a7bcbc41876be21d6590af6f
HDDTHR <stevenjildeh@gmail.com>
2024-02-29 17:26:37 +0300

    Add prevent-duplicate-exams extension

---
+++ src/prevent-duplicate-exams/query.ts
@@ -0,0 +1,28 @@
+import { VALID_EXAM_TYPES } from './constants';
+
+// Query that is used to find conflicts
+export default (payload: any) => ({
+	fields: ['id'],
+	filter: {
+		course: {
+			_eq: payload.course,
+		},
+		semester: {
+			_and: {
+				_eq: payload.semester[0].toUpperCase() + payload.semester.slice(1),
+				_nnull: true,
+			},
+		},
+		year: {
+			_eq: payload.year,
+		},
+		exam_data: {
+			type: {
+				_and: {
+					_eq: payload.exam_data.type,
+					_in: VALID_EXAM_TYPES,
+				},
+			},
+		},
+	},
+});
