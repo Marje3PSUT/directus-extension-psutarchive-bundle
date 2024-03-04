f1e7338c23447d59e31d6a478a4a2b2aced72721
HDDTHR <stevenjildeh@gmail.com>
2024-03-04 12:10:45 +0300

    Fix query for resource conflict hook

--- src/prevent-duplicate-exams/query.ts
+++ src/prevent-duplicate-exams/query.ts
@@ -1,28 +1,51 @@
-import { VALID_EXAM_TYPES } from '../utils/constants';
-
+import { VALID_EXAM_CONFLICT_TYPES } from '../utils/constants';
 // Query that is used to find conflicts
 export default (payload: any) => ({
 	fields: ['id'],
 	filter: {
-		course: {
-			_eq: payload.course,
-		},
-		semester: {
-			_and: {
-				_eq: payload.semester.toUpperCase() + payload.semester.slice(1),
-				_nnull: true,
-			},
-		},
-		year: {
-			_eq: payload.year,
-		},
-		exam_data: {
-			type: {
-				_and: {
-					_eq: payload.exam_data.type,
-					_in: VALID_EXAM_TYPES,
+		_and: [
+			{
+				course: {
+					_eq: payload.course,
 				},
 			},
-		},
+			{
+				year: {
+					_eq: payload.year,
+				},
+			},
+			{
+				_and: [
+					{
+						semester: {
+							_nnull: true,
+						},
+					},
+					{
+						semester: {
+							_eq: payload.semester ? payload.semester : null,
+						},
+					},
+				],
+			},
+			{
+				_and: [
+					{
+						exam_data: {
+							type: {
+								_eq: payload.exam_data.type,
+							},
+						},
+					},
+					{
+						exam_data: {
+							type: {
+								_in: VALID_EXAM_CONFLICT_TYPES,
+							},
+						},
+					},
+				],
+			},
+		],
 	},
 });
