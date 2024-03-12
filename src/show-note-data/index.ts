6bd5d3d691a4a1eb8b45b3aeb872926a9677af91
HDDTHR <stevenjildeh@gmail.com>
2024-03-13 00:52:34 +0300

    Fix unintended behaviour in show-note-data hook

--- src/show-note-data/index.ts
+++ src/show-note-data/index.ts
@@ -7,23 +7,49 @@
 import { defineHook } from '@directus/extensions-sdk';
 import _ from 'lodash';
 import { CONTRIBUTOR_ROLE_NAME } from '../utils/constants';
+import { EventContext } from '@directus/types';
 
 export default defineHook(({ filter }, { services }) => {
-	const { ItemsService } = services;
+	filter('resource.items.read', async (payload, meta, context) => {
+		return await clean(payload, context, services, cleanResourceEntities);
+	});
 
-	filter('resource.items.read', async (payload: any, meta, { accountability, schema, database }) => {
-		if (!accountability) return payload;
-
-		const userItemsService = new ItemsService('directus_users', { database, schema });
-		const userEntity = await userItemsService.readOne(accountability.user, { fields: ['role.name'] });
-
-		if (userEntity.role.name != CONTRIBUTOR_ROLE_NAME) return payload;
-
-		const cleanResources = payload.map((resource) => {
-			if (resource.type == 'note') return resource;
-			return _.omit(resource, ['user_created', 'user_updated']);
-		});
-
-		return cleanResources;
+	filter('course.items.read', async (payload, meta, context) => {
+		return await clean(payload, context, services, cleanCourseEntities);
 	});
 });
+
+const clean = async (payload: any, context: EventContext, services: any, cleanHandler: (payload: any) => any) => {
+	const { accountability, database, schema } = context;
+	const { ItemsService } = services;
+
+	if (!accountability || accountability.role === null) return cleanHandler(payload);
+	if (accountability.admin) return payload;
+
+	const userItemsService = new ItemsService('directus_users', {
+		database,
+		schema,
+	});
+
+	const userEntity = await userItemsService.readOne(accountability.user, {
+		fields: ['role.name'],
+	});
+
+	if (userEntity.role.name === CONTRIBUTOR_ROLE_NAME) return cleanHandler(payload);
+
+	return payload;
+};
+
+const cleanResourceEntities = (payload: any) =>
+	payload.map((resource) => {
+		if (resource.type == 'note') return resource;
+		return _.omit(resource, ['user_created', 'user_updated']);
+	});
+
+const cleanCourseEntities = (payload: any) => {
+	return payload.map((course) => {
+		if (course.resource) course.resource = cleanResourceEntities(course.resource);
+
+		return course;
+	});
+};
